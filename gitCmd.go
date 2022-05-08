1834cbe3b28be133e9cbd72dc04bf213399ea40f
John, Sing Dao, Siu <john.sd.siu@gmail.com>
2022-05-08 03:22:28 -0400

    - Add workpath support  - Add GitRoot(), GitRootSubmodule()  - Add GitExecExist(), GitExecPath()

--- gitCmd.go
+++ gitCmd.go
@@ -22,52 +22,107 @@ THE SOFTWARE.
 
 package helper
 
+import (
+	"os/exec"
+	"strings"
+)
+
+// Check git executable exit
+func GitExecExist() bool {
+	return GitExecPath() != ""
+}
+
+// Check git executable path
+func GitExecPath() string {
+	path, err := exec.LookPath("git")
+	if err != nil {
+		return ""
+	}
+	return path
+}
+
+// execute "git init" using MyCmd
+func GitInit(workpathP *string) *MyCmd {
+	return MyCmdRun("git", &[]string{"init"}, workpathP)
+}
+
+// execute "git push" using MyCmd
+func GitPush(workpathP *string, optionsP []string) *MyCmd {
+	args := []string{"push"}
+	if optionsP != nil {
+		args = append(args, optionsP...)
+	}
+	return MyCmdRun("git", &args, workpathP)
+}
+
 // execute "git remote" using MyCmd
-func GitRemote(v bool) *[]string {
+func GitRemote(workpathP *string, v bool) *[]string {
 	var args []string
 	if v {
 		args = []string{"remote", "-v"}
 	} else {
 		args = []string{"remote"}
 	}
-	output := MyCmdRun("git", &args).Stdout.String()
+	output := MyCmdRun("git", &args, workpathP).Stdout.String()
 	return StrPToArrayP(&output)
 }
 
-// execute "git init" using MyCmd
-func GitInit() *MyCmd {
-	return MyCmdRun("git", &[]string{"init"})
-}
-
-// execute "git push" using MyCmd
-func GitPush(optionsP []string) *MyCmd {
-	args := []string{"push"}
-	if optionsP != nil {
-		args = append(args, optionsP...)
-	}
-	return MyCmdRun("git", &args)
-}
-
 // execute "git remote add" using MyCmd
-func GitRemoteAdd(name string, git string) *MyCmd {
-	return MyCmdRun("git", &[]string{"remote", "add", name, git})
+func GitRemoteAdd(workpathP *string, name string, git string) *MyCmd {
+	return MyCmdRun("git", &[]string{"remote", "add", name, git}, workpathP)
 }
 
 // execute "git remote exit" using MyCmd
-func GitRemoteExist(name string) bool {
-	r := GitRemote(false)
+func GitRemoteExist(workpathP *string, name string) bool {
+	r := GitRemote(workpathP, false)
 	return StrArrayPtrContain(r, &name)
 }
 
 // execute "git remote remove" using MyCmd
-func GitRemoteRemove(name string) *MyCmd {
-	return MyCmdRun("git", &[]string{"remote", "remove", name})
+func GitRemoteRemove(workpathP *string, name string) *MyCmd {
+	return MyCmdRun("git", &[]string{"remote", "remove", name}, workpathP)
 }
 
 // execute "git remote remove" all git remote using MyCmd
-func GitRemoteRemoveAll() {
-	gr := GitRemote(false)
+func GitRemoteRemoveAll(workpathP *string) {
+	gr := GitRemote(workpathP, false)
 	for _, r := range *gr {
-		GitRemoteRemove(r)
+		GitRemoteRemove(workpathP, r)
 	}
+}
+
+// Get git root from current directory.
+// Return empty string if not a git dir.
+func GitRoot(workpathP *string) string {
+	if *workpathP == "" {
+		CurrentPath()
+	}
+	// Check submodule path repeatly
+	var submodulePath string = *workpathP
+	var currPath string = *workpathP
+	for submodulePath != "" {
+		submodulePath = GitRootSubmodule(&submodulePath)
+		if submodulePath != "" {
+			currPath = submodulePath
+		}
+	}
+	// Check git root
+	var opts []string = []string{"rev-parse", "--show-toplevel"}
+	var myCmd *MyCmd = MyCmdRun("git", &opts, &currPath)
+	if myCmd.Err != nil {
+		return ""
+	}
+	return strings.TrimSpace(myCmd.Stdout.String())
+}
+
+// Get git submodule root from `workpath`.
+// If `workpath` is empty, current directory is used.
+// Return empty string if not a submodule dir.
+func GitRootSubmodule(workpathP *string) string {
+	var opts []string = []string{"rev-parse", "--show-superproject-working-tree"}
+	var myCmd *MyCmd = MyCmdRun("git", &opts, workpathP)
+	if myCmd.Err != nil {
+		return ""
+	}
+	return strings.TrimSpace(myCmd.Stdout.String())
 }
