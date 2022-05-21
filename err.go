12fb3a029217d5d92d8cbaccc2ba9989e27b2c67
John, Sing Dao, Siu <john.sd.siu@gmail.com>
2022-05-21 14:24:40 -0400

    Add ErrsT.Empty(), ErrsT.Clear()

--- err.go
+++ err.go
@@ -1,25 +1,37 @@
 package helper
 
-// Err is a string with error interface
+// Type Err is a string with error interface
 type Err string
 
-// ErrArrayT is array of errors
+// Type ErrT is array of errors
 type ErrsT []error
 
-// `error` interface`
+// `error` interface
 func (self Err) Error() string {
 	return string(self)
 }
 
-// `stringer` interface
+// Return content of Err as string
 func (self *Err) String() string {
 	return string(*self)
 }
 
+// Return content of Err as *string
 func (self *Err) StringP() *string {
 	return (*string)(self)
 }
 
+// Return true if ErrsT array is empty
+func (self *ErrsT) Empty() bool {
+	return len(*self) == 0
+}
+
+// Return true if ErrsT array is not empty
 func (self *ErrsT) NotEmpty() bool {
 	return len(*self) > 0
+}
+
+// Clear the ErrsT array
+func (self *ErrsT) Clear() {
+	self = &ErrsT{}
 }
