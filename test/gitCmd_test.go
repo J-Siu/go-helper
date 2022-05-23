5173223621fb7ccf13e68e31060b478581f2d568
John, Sing Dao, Siu <john.sd.siu@gmail.com>
2022-05-23 03:27:00 -0400

    Add proper clean up

--- test/gitCmd_test.go
+++ test/gitCmd_test.go
@@ -13,17 +13,32 @@ func TestGitRoot_hasSubmodule(t *testing.T) {
 
 	// Pull submodules
 	var args []string
-	//   git submodule add https://github.com/J-Siu/submodule_test_1
-	args = []string{"submodule", "add", "https://github.com/J-Siu/submodule_test_1"}
-	helper.MyCmdRun("git", &args, nil)
+	var myCmd *helper.MyCmd
+	//		git submodule add -f https://github.com/J-Siu/submodule_test_1
+	args = []string{"submodule", "add", "-f", "https://github.com/J-Siu/submodule_test_1"}
+	myCmd = helper.MyCmdRun("git", &args, nil)
+	helper.Report(myCmd, "myCmd", false, false)
+	helper.Report(myCmd.Stderr, "myCmd.Stderr", true, false)
+	helper.Report(myCmd.Stdout, "myCmd.Stdout", true, false)
 	//   git submodule update --recursive --init
 	args = []string{"submodule", "update", "--recursive", "--init"}
-	helper.MyCmdRun("git", &args, nil)
+	myCmd = helper.MyCmdRun("git", &args, nil)
+	helper.Report(myCmd, "myCmd", false, false)
+	helper.Report(myCmd.Stderr, "myCmd.Stderr", true, false)
+	helper.Report(myCmd.Stdout, "myCmd.Stdout", true, false)
 
-	// testDir should be set to a git submodule directory.
+	// Test
+	//		testDir should be set to a git submodule directory.
 	var testDir string = "submodule_test_1/submodule_test_2/submodule_test_3/submodule_test_4/submodule_test_5"
 	var wanted string = "go-helper"
 	var msg string = path.Base(helper.GitRoot(&testDir))
+
+	// Clean up
+	//		git rm -rf submodule_test_1 ../.gitmodules
+	args = []string{"rm", "-rf", "submodule_test_1", "../.gitmodules"}
+	myCmd = helper.MyCmdRun("git", &args, nil)
+
+	// Result
 	fmt.Println("Git root of " + testDir + " is `" + msg + "`")
 	if msg != wanted {
 		t.Fatalf(`GitRoot(rtSp(%s) = ""`, testDir)
