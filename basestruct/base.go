17f77d0434c573dac7673dbc7c81897c42b8be5c
John, Sing Dao, Siu <john.sd.siu@gmail.com>
2026-01-04 11:45:22 -0500

    update license to 2026

--- basestruct/base.go
+++ basestruct/base.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2025 John, Sing Dao, Siu <john.sd.siu@gmail.com>
+Copyright © 2026 John, Sing Dao, Siu <john.sd.siu@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
